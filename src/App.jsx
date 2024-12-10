4d7af80819a677ebf45181e2584639e454e408bc
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 00:44:50 +0545

    Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Card from "./components/Card";
 function App() {
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen">
-                <div className="ring-offset-4 ring-8 ring-pink-500 w-64 h-64 bg-green-500 border-4 border-blue-700 rounded-3xl"></div>
-            </div>
+            <h1 className="bg-purple-500 md:bg-green-500 lg:bg-red-500 p-10">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos, iure?
+            </h1>
         </>
     );
 }
