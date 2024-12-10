dc81031297e7b7270f21e3c6329060f997ab51e1
Subash Gauatm <subashgautam977@gmail.com>
2024-12-10 23:44:11 +0545

    {/* by customization  */}

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function App() {
     return (
         <>
+            {/* by customization  */}
             <h1 className="">This is first test over tailwind css.</h1>
+            {/* After overridden  */}
+            <h1 className="text-2xl text-pink-500 mt-5">
+                This is first test over tailwind css.
+            </h1>
         </>
     );
 }
