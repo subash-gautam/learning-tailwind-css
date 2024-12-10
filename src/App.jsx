82ede6802904ea79e8d50bf1dafcb5802917dd86
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 00:02:16 +0545

    bg gradients

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
+import Card from "./components/Card";
 
 function App() {
     return (
         <>
-            <p className="text-cyan-400 bg-indigo-500 text-5xl p-5">
-                Tailwind color classs:
-            </p>
-            <p className="text-3xl">
-                Background (bg-COL-STR) eg: bg-indigo-500
-            </p>
-            <p className="text-3xl">Text (text-COL-STR) eg: text-cyan-300</p>
+            <div className="grid place-items-center m-5 p-5">
+                <Card />
+            </div>
         </>
     );
 }
