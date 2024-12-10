d77f463de75b8d68fb23c74092ef1f77f3a7ecd0
Subash Gauatm <subashgautam977@gmail.com>
2024-12-10 23:40:56 +0545

    Customizing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function App() {
     return (
         <>
-            <h1 className="text-4xl text-indigo-900 bg-blue-200 py-5 px-10">
-                This is first test over tailwind css.
-            </h1>
+            <h1 className="">This is first test over tailwind css.</h1>
         </>
     );
 }
