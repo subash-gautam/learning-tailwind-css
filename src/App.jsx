018ef35358833c5e2ac962742d06bd2ce8aa7124
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 00:49:07 +0545

    Chapter 3 finished : Utility first features

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import Card from "./components/Card";
 
 function App() {
-    return (
-        <>
-            <h1 className="bg-purple-500 md:bg-green-500 lg:bg-red-500 p-10">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Dignissimos, iure?
-            </h1>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
