f9bed5277a434a63bb12bf71ab48aeb766d01678
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 00:25:51 +0545

    Box Shadow

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Card from "./components/Card";
 function App() {
     return (
         <>
-            <p class="font-sans hover:font-serif focus:text-mono text-4xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Reprehenderit, sequi.
-            </p>
-            <p class="font-sans ...">sans: The quick brown fox ...</p>
-            <p class="font-serif ...">serif: The quick brown fox ...</p>
-            <p class="font-mono ...">mono: The quick brown fox ...</p>
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="shadow-lg w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5 text-center">
+                    Box Shadow shadow(-AMT)
+                </div>
+                <div className="shadow-inner w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5 text-center">
+                    {" "}
+                    AMT none inner sm md lg xl 2xl
+                </div>
+                <div className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5"></div>
+            </div>
         </>
     );
 }
