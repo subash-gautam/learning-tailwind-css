d5c5203a3fba812430f76a57bccbb30b71cc13c8
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 00:29:55 +0545

    Screen Readers sr-only and not-sr-only

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import Card from "./components/Card";
 function App() {
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen">
-                <div className="shadow-lg w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5 text-center">
-                    Box Shadow shadow(-AMT)
-                </div>
-                <div className="shadow-inner w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5 text-center">
-                    {" "}
-                    AMT none inner sm md lg xl 2xl
-                </div>
-                <div className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] w-64 h-64 bg-white-100 border-4 border-green-300 rounded-3xl m-5"></div>
-            </div>
+            <a href="#">
+                {/* Setting 1 to be hidden */}
+                <span class="sr-only">Settings 1</span>
+                {/* Setting 2 not to be hidden */}
+                <span class="not-sr-only">Settings 2</span>
+            </a>
         </>
     );
 }
