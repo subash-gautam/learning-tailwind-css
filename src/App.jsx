c52a2c923fe7876a4199f435ea86fee04c4968dd
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 17:59:19 +0545

    Link : active, visited and target <-- interesting

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,46 @@ import Button from "./components/Button";
 function App() {
     return (
         <>
-            <div className=" grid place-items-center p-5">
-                <Button
-                    label="Focus"
-                    className={"focus:ring-4 focus:bg-indigo-600"}
-                />{" "}
-                <div className="focus-within:bg-cyan-600 focus-within:text-white">
-                    <Button label="Focus-within" />
-                </div>
-                <Button
-                    label="Focus-visible : Only work when navigating with keyboard...."
-                    className="focus-visible:ring-4 focus-visible:bg-indigo-600"
-                />
+            <div>
+                <a
+                    href="#1"
+                    className="active:bg-blue-500 visited:bg-green-600">
+                    one
+                </a>
+                <a
+                    href="#2"
+                    className="active:bg-blue-500 visited:bg-green-600">
+                    two
+                </a>
+                <a
+                    href="#3"
+                    className="active:bg-blue-500 visited:bg-green-600">
+                    three
+                </a>
+            </div>
+            <div id="1" className="target:bg-slate-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+                quis beatae iure voluptate illo fuga expedita similique vero
+                recusandae aut quaerat assumenda eum placeat facilis ipsam
+                aliquam? Magni, laborum atque! Odit, quas totam quod et
+                provident quibusdam at distinctio dolorum natus incidunt sed
+                quis nisi ut voluptatum eaque, necessitatibus magni.
+            </div>
+            <div id="2" className="target:bg-slate-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+                quis beatae iure voluptate illo fuga expedita similique vero
+                recusandae aut quaerat assumenda eum placeat facilis ipsam
+                aliquam? Magni, laborum atque! Odit, quas totam quod et
+                provident quibusdam at distinctio dolorum natus incidunt sed
+                quis nisi ut voluptatum eaque, necessitatibus magni.
+            </div>
+            <div id="3" className="target:bg-slate-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+                quis beatae iure voluptate illo fuga expedita similique vero
+                recusandae aut quaerat assumenda eum placeat facilis ipsam
+                aliquam? Magni, laborum atque! Odit, quas totam quod et
+                provident quibusdam at distinctio dolorum natus incidunt sed
+                quis nisi ut voluptatum eaque, necessitatibus magni.
             </div>
         </>
     );
