bf7a8d0aca98811a6ca5f7df4e46cabf35160395
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 22:27:32 +0545

    peer

--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,27 @@ import Button from "./components/Button";
 function App() {
     return (
         <>
-            <main className="flex w-screen h-screen flex flex-col items-center justify-center">
-                <footer id="social" className="target:pt-24 mx-auto pt-16 pb-6">
-                    <h1 className="text-center font-semibold tracking-wider text-indigo-600 uppercase">
-                        Social Media : Group groups the childs and can be used
-                        to style a particular child when and effects on parent
-                        is happened.
-                        <br />
-                        For example if We hover over any icon, its text color
-                        changes because icon and div containing text are under a
-                        tag which i grouped.
-                    </h1>
-                    <div className="mt-6 flex place-content-center gap-6 text-center">
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-linkedin text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                LinkedIn
-                            </div>
-                        </a>
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-twitter text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                Twitter
-                            </div>
-                        </a>
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-youtube text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                YouTube
-                            </div>
-                        </a>
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-snapchat text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                SnapChat
-                            </div>
-                        </a>
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-instagram text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                Instagram
-                            </div>
-                        </a>
-                        <a href="#" className="group text-gray-400">
-                            <i className="bi bi-tiktok text-2xl"></i>
-                            <div className="hidden md:inline ml-1 group-hover:text-indigo-500">
-                                TikTok
-                            </div>
-                        </a>
+            <form class="w-full grid gap-2 px-4">
+                <div class="flex justify-between items-center">
+                    <label
+                        for="name"
+                        class="w-32 text-right pr-4 font-bold text-gray-700">
+                        Name
+                    </label>
+                    <div class="flex-1">
+                        <input
+                            required
+                            placeholder="name"
+                            type="text"
+                            id="name"
+                            class="peer mt-5 invalid:border-red-300 invalid:border-2 w-full rounded-md appearance-none border border-gray-300 py-2 px-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent"
+                        />
+                        <p class="hidden peer-invalid:block peer-invalid:font-bold peer-invalid:text-red-500">
+                            A name is required.
+                        </p>
                     </div>
-                </footer>
-            </main>
+                </div>
+            </form>
         </>
     );
 }
