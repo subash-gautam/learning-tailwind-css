62d7d31bc7f809c7e26ff38d5a874864e633d36b
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 18:10:22 +0545

    only and only-of-type

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ function App() {
     return (
         <>
             <div>
-                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400">
+                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Expedita, quibusdam.
                 </p>
-                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400">
+                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Expedita, quibusdam.
                 </a>
-                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Expedita, quibusdam.
-                </p>
-                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400">
+                <div className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
+                    Highlights on only-of-type.there exist only too, it works
+                    when there is only one element
+                </div>
+                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Expedita, quibusdam.
                 </a>
-                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400">
+                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Expedita, quibusdam.
                 </p>
