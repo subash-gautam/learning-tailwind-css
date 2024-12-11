5f5aa528479b9e8d2aee5e629a99afc0f41fe813
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 17:12:19 +0545

    hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Card from "./components/Card";
+import Button from "./components/Button";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <div className=" grid place-items-center p-5">
+                <Button className="hover:bg-cyan-700 hover:border-cyan-950 hover:text-red-300" />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+// BeautifulButton.js
+import React from "react";
+
+const Button = ({ label = "Default label", className }) => {
+    console.log(className);
+    return (
+        <button
+            className={`${className} font-bold py-2 px-4 rounded
+            bg-blue-600 border-4 border-blue-950 text-white`}>
+            {label}
+        </button>
+    );
+};
+
+export default Button;
