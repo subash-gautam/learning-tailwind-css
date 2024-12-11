c350613b54298a29c2c78d749aac9bc156897041
Subash Gauatm <subashgautam977@gmail.com>
2024-12-12 00:41:29 +0545

    Course finished :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,7 @@ import Card from "./components/Card";
 import Button from "./components/Button";
 
 function App() {
-    return (
-        <>
-            <p className="text-black dark:bg-black dark:text-white">
-                In normal light mode, there is black text with white background.
-                When the system's mode is set to be dark the dart:[style] come
-                to in play. For this case, bg to be black and text to be light.
-            </p>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
