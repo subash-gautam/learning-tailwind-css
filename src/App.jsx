26d99f0e3630e574062f19b9fe74245e433f1c05
Subash Gauatm <subashgautam977@gmail.com>
2024-12-11 18:17:10 +0545

    order : odd, even

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,23 @@ function App() {
     return (
         <>
             <div>
-                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Expedita, quibusdam.
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
                 </p>
-                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Expedita, quibusdam.
-                </a>
-                <div className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
-                    Highlights on only-of-type.there exist only too, it works
-                    when there is only one element
-                </div>
-                <a className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Expedita, quibusdam.
-                </a>
-                <p className="first-of-type:bg-blue-500 last-of-type:bg-green-400 only-of-type:text-white">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Expedita, quibusdam.
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p className="odd:bg-green-400 odd:text-blue-500 even:bg-blue-500 even:text-green-500">
+                    Lorem ipsum dolor sit amet.
                 </p>
             </div>
         </>
